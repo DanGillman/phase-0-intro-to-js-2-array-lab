9c7c08f8b1eb6773b824783ee1ae8796d100e73f
dangillman <dangillman93@gmail.com>
2023-12-07 15:10:42 -0500

    completed labs, added content to index.js file.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+  ];
+function destructivelyAppendCat(Garfield) {
+    cats.push("Ralph");
+    }
+function destructivelyPrependCat(Milo) {
+    cats.unshift("Bob");
+    }
+function destructivelyRemoveLastCat(Milo) {
+    cats.pop(Milo);
+    }
+function destructivelyRemoveFirstCat(Garfield) {
+    cats.shift("Garfield");
+    }
+function appendCat(Broom) {
+    return cats.concat("Broom");
+    }
+      
+    const newCats = appendCat(cats, "Broom");
+function prependCat(Arnold) {
+        return [Arnold, ...cats];
+      }
+function removeLastCat() {
+        return cats.slice(0, -1);
+      }      
+function removeFirstCat() {
+        return cats.slice(1);
+      }
